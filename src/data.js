8195ff777f3a94c5ec18750880e57d240e4011a1
tushar ghodadra <tusharghodadra@gmail.com>
2019-10-30 11:31:46 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Devotional",
+    img: "img/2e131bd25c3af15a9d82a4f420775f19.jpg",
+    price: 12,
+    company: "jarabara,merigold",
+    info:
+      "This mixed colored garland made of pink jarbar and marigol is perfectly enchanting. Adorned with jarbara leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Engagement",
+    img: "img/6cb309178f230e51fbeacb2387aeee02.jpg",
+    price: 16,
+    company: "merigold,roses",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Engagement memorable. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Wedding",
+    img: "img/715aafb4eda3b943b06bde9a96ebbfb0.jpg",
+    price: 18,
+    company: "merigold,roses",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Wedding  memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Greetings",
+    img: "img/flower-garlands-250x250.jpg",
+    price: 18,
+    company: "roses ,lili",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Get Together memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Greetings",
+    img: "img/Button-ToseGarland-BG-03.jpg",
+    price: 24,
+    company: "roses",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Get Together memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Wedding",
+    img: "img/download.jpg",
+    price: 17,
+    company: "roses,lili,merigold",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Wedding memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Devotional",
+    img: "img/flower-garland-500x500.jpg",
+    price: 12,
+    company: "merigold ,rosa moschata,roses",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Devotional memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Wedding",
+    img: "img/1660efd84893fc1396a4b35256bd9073.jpg",
+    price: 17,
+    company: "roses,rosa moschata",
+    info:
+      "This mixed colored garland made of red Roses and marigol is perfectly enchanting. Adorned with Roses leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment And make Your Wedding memorable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Devotional",
+  img: "img/2e131bd25c3af15a9d82a4f420775f19.jpg",
+  price: 12,
+  company: "jarabara,merigold",
+  info:
+    "This mixed colored garland made of pink jarbar and marigol is perfectly enchanting. Adorned with jarbara leaves this garland is created with traditional beauty. Embedded with blessings and touch of love, it will create a blissful and memorable moment",
+  inCart: false,
+  count: 0,
+  total: 0
+};
